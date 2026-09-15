6f1bdd654ae005d7980d05f3614942e9ed1d78c6
kucoinzendesk-cpu <kucoin.zendesk@gmail.com>
2026-09-15 20:41:36 +0100

    Update chains config to include all major EVM chains

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -1,4 +1,5 @@
 export const CHAIN_CONFIG: Record<number, { name: string; symbol: string; blockExplorer: string; rpc?: string }> = {
+  // Ethereum
   1: {
     name: 'Ethereum',
     symbol: 'ETH',
@@ -6,23 +7,19 @@ export const CHAIN_CONFIG: Record<number, { name: string; symbol: string; blockE
     rpc: process.env.ETH_RPC_URL,
   },
   11155111: {
-    name: 'Sepolia',
+    name: 'Sepolia Testnet',
     symbol: 'ETH',
     blockExplorer: 'https://sepolia.etherscan.io',
   },
-  137: {
-    name: 'Polygon',
-    symbol: 'MATIC',
-    blockExplorer: 'https://polygonscan.com',
-    rpc: process.env.POLYGON_RPC_URL,
-  },
+
+  // Layer 2s
   10: {
     name: 'Optimism',
     symbol: 'ETH',
     blockExplorer: 'https://optimistic.etherscan.io',
   },
   42161: {
-    name: 'Arbitrum',
+    name: 'Arbitrum One',
     symbol: 'ETH',
     blockExplorer: 'https://arbiscan.io',
   },
@@ -31,10 +28,155 @@ export const CHAIN_CONFIG: Record<number, { name: string; symbol: string; blockE
     symbol: 'ETH',
     blockExplorer: 'https://basescan.org',
   },
+
+  // Sidechains
+  137: {
+    name: 'Polygon',
+    symbol: 'MATIC',
+    blockExplorer: 'https://polygonscan.com',
+    rpc: process.env.POLYGON_RPC_URL,
+  },
   43114: {
-    name: 'Avalanche',
+    name: 'Avalanche C-Chain',
     symbol: 'AVAX',
     blockExplorer: 'https://snowscan.xyz',
+  },
+  56: {
+    name: 'BNB Smart Chain',
+    symbol: 'BNB',
+    blockExplorer: 'https://bscscan.com',
+  },
+
+  // Other EVM Chains
+  250: {
+    name: 'Fantom',
+    symbol: 'FTM',
+    blockExplorer: 'https://ftmscan.com',
+  },
+  1088: {
+    name: 'Metis',
+    symbol: 'METIS',
+    blockExplorer: 'https://explorer.metis.io',
+  },
+  42220: {
+    name: 'Celo',
+    symbol: 'CELO',
+    blockExplorer: 'https://celoscan.io',
+  },
+  1313161554: {
+    name: 'Aurora',
+    symbol: 'ETH',
+    blockExplorer: 'https://explorer.aurora.dev',
+  },
+  25: {
+    name: 'Cronos',
+    symbol: 'CRO',
+    blockExplorer: 'https://cronoscan.com',
+  },
+  1101: {
+    name: 'Polygon zkEVM',
+    symbol: 'ETH',
+    blockExplorer: 'https://zkevm.polygonscan.com',
+  },
+  324: {
+    name: 'zkSync Era',
+    symbol: 'ETH',
+    blockExplorer: 'https://explorer.zksync.io',
+  },
+  59144: {
+    name: 'Linea',
+    symbol: 'ETH',
+    blockExplorer: 'https://lineascan.build',
+  },
+  534352: {
+    name: 'Scroll',
+    symbol: 'ETH',
+    blockExplorer: 'https://scrollscan.com',
+  },
+  100: {
+    name: 'Gnosis Chain',
+    symbol: 'xDAI',
+    blockExplorer: 'https://gnosisscan.io',
+  },
+  128: {
+    name: 'Huobi ECO Chain',
+    symbol: 'HT',
+    blockExplorer: 'https://hecoinfo.com',
+  },
+  66: {
+    name: 'OKX Chain',
+    symbol: 'OKT',
+    blockExplorer: 'https://www.oklink.com/okc',
+  },
+  1666600000: {
+    name: 'Harmony One',
+    symbol: 'ONE',
+    blockExplorer: 'https://explorer.harmony.one',
+  },
+  122: {
+    name: 'Fuse',
+    symbol: 'FUSE',
+    blockExplorer: 'https://explorer.fuse.io',
+  },
+  1284: {
+    name: 'Moonbeam',
+    symbol: 'GLMR',
+    blockExplorer: 'https://moonscan.io',
+  },
+  1285: {
+    name: 'Moonriver',
+    symbol: 'MOVR',
+    blockExplorer: 'https://moonriver.moonscan.io',
+  },
+  361: {
+    name: 'Theta',
+    symbol: 'THETA',
+    blockExplorer: 'https://explorer.thetatoken.org',
+  },
+  1030: {
+    name: 'Conflux eSpace',
+    symbol: 'CFX',
+    blockExplorer: 'https://evm.confluxscan.io',
+  },
+  2000: {
+    name: 'Dogechain',
+    symbol: 'DOGE',
+    blockExplorer: 'https://explorer.dogechain.dog',
+  },
+  7700: {
+    name: 'Canto',
+    symbol: 'CANTO',
+    blockExplorer: 'https://evm.explorer.canto.io',
+  },
+  1088: {
+    name: 'Metis Andromeda',
+    symbol: 'METIS',
+    blockExplorer: 'https://andromeda-explorer.metis.io',
+  },
+  33: {
+    name: 'Aves',
+    symbol: 'AVES',
+    blockExplorer: 'https://explorer.avescoin.io',
+  },
+  4689: {
+    name: 'IoTeX',
+    symbol: 'IOTX',
+    blockExplorer: 'https://iotexscan.io',
+  },
+  9001: {
+    name: 'Evmos',
+    symbol: 'EVMOS',
+    blockExplorer: 'https://escan.live',
+  },
+  322: {
+    name: 'KCC',
+    symbol: 'KCS',
+    blockExplorer: 'https://explorer.kcc.io',
+  },
+  1116: {
+    name: 'Core',
+    symbol: 'CORE',
+    blockExplorer: 'https://explorer.coredao.org',
   },
 };
 
